48f61863c5a1c9ae9130fac053f18981e6a18d57
kwleiker <kwleiker@gmail.com>
2020-01-28 14:49:52 -0600

    Add files via upload

---
+++ static/js/config.js
@@ -0,0 +1,8 @@
+// git.ignore 
+//API Keys  
+const osm_key = "{osm_key}"; 
+const API_KEY = "{private}"; 
+const mapbox_key = "{private mapbox key}"; 
+const public_token = "pk.eyJ1Ijoia3dsZWlrZXIiLCJhIjoiY2s1eGE1bWZ2MDJ3YjNudW12dXRrdTg3MCJ9.rKDEVW8henYC6k0_btcuYw"; 
+
+
